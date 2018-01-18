8ad73551384b205f6b5a39479c973bee77d5681d
Wynn Chen <wynn987@gmail.com>
2018-01-18 14:57:49 +0800

    Removed header and improved styling

--- src/App.js
+++ src/App.js
@@ -8,37 +8,7 @@ import {
 import Routes from "./Routes";
 
 const Header = () => (
-  <div>
-    <a href="/" style={{
-      color: "white"
-    }}>
-      <div className="left-sidebar" style={{
-        backgroundColor: "#4A8EE0",
-        textAlign: "center",
-        paddingLeft: "0"
-      }}>
-        <h1 style={{
-          fontSize: "60px",
-          marginBottom: "0"
-        }}>WYNN</h1>
-        <p style={{marginTop: "0", marginBottom: "2em"}}>Demo App</p>
-      </div>
-    </a>
-    <div className="main-content">
-      <div style={{
-        textAlign: "right",
-        marginBottom: "3em"
-      }}>
-      </div>
-      <div>
-        <Row>
-          <Col xsOffset={4} xs={2} style={{ textAlign: "right" }}>
-            <Button bsStyle="primary">Logout</Button>
-          </Col>
-        </Row>
-      </div>
-      <hr />
-    </div>
+  <div style={{textAlign: "center"}}>
   </div>
 );
 
--- src/auth/SignIn/SignIn.jsx
+++ src/auth/SignIn/SignIn.jsx
@@ -1,42 +1,46 @@
 import React, {Component} from "react";
-import { Control, Form, actions } from 'react-redux-form';
+import {Control, Form} from 'react-redux-form';
 import "./SignIn.css";
-import {API_URL_PREFIX} from '../../utilities/helper';
 import {connect} from 'react-redux';
 import {authUser} from './SignInActions';
 
 class Login extends Component {
 
   handleSubmit(user) {
-    this.props.authUser(user);
+    this
+      .props
+      .authUser(user);
   }
 
   render() {
     return (
-      <Form
-      model="user"
-      onSubmit={(user) => this.handleSubmit(user)}
-    >
-      <label htmlFor="user.email">Email:</label>
-      <Control type='email' model="user.email" id="user.email" />
-
-      <label htmlFor="user.password">Password:</label>
-      <Control type='password' model="user.password" id="user.password" />
-
-      <button type="submit">
-        Login
-      </button>
-    </Form>
+      <div class='login-page'>
+        <Form class='form' model="user" onSubmit={(user) => this.handleSubmit(user)}>
+          <div class='row'>
+            <label htmlFor="user.email">Email:</label>
+            <Control type='email' model="user.email" id="user.email"/>
+          </div>
+          <div class='row'>
+            <label htmlFor="user.password">Password:</label>
+            <Control
+              type='password'
+              model="user.password"
+              id="user.password"/>
+          </div>
+          <div class='row'>
+          <br/>
+            <button type="submit">
+              Login
+            </button>
+          </div>
+        </Form>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    user: state.user, 
-    hasErrored: state.userHasErrored, 
-    authenticating: state.authenticating
-  };
+  return {user: state.user, hasErrored: state.userHasErrored, authenticating: state.authenticating};
 };
 
 const mapDispatchToProps = (dispatch) => {
