891d4453c2ca640b9ff3c5c18b3232aa07dec821
Wynn Chen <wynn987@gmail.com>
2018-01-19 13:53:47 +0800

    added redux logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import logger from 'redux-logger'
+
 export default function configureStore(initialState) {
+    const middlewares = [];
+
+    middlewares.push(thunk);
+
+    if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+
+    middlewares.push(logger);
+    }
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        applyMiddleware(...middlewares)
     );
 }
